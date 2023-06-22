d4d07376f3e54731635af10aa880e86c201ecd4b
buttowsk <vinicius_nunes0104@hotmail.com>
2023-06-22 17:51:11 -0300

    popularity complete

--- src/graph.cr
+++ src/graph.cr
@@ -14,7 +14,7 @@ def get_expanded_info(ids)
           type
           dimension
           residents {
-            episode{
+            episode {
               name
             }
           }
@@ -27,15 +27,7 @@ def get_expanded_info(ids)
           name: data["location"]["name"],
           type: data["location"]["type"],
           dimension: data["location"]["dimension"],
-          residents: data["location"]["residents"]
-        }
-      else
-        {
-          id: id,
-          name: "unknown",
-          type: "unknown",
-          dimension: "unknown",
-          residents: "unknown"
+          residents: data["location"]["residents"].as_a
         }
       end
     rescue
@@ -52,19 +44,24 @@ def get_expanded_info(ids)
 end
 
 
-def calcular_popularidade(stops_infos)
-  popularity = stops_infos.map do |info|
-    info["residents"].size
-  end 
+def calc_popularity(stops_infos)
+  parse = JSON.parse(stops_infos).as_a
+  popularity = parse.map do |info|
+    sum = info["residents"].as_a.map do |resident|
+      resident["episode"].size
+    end.sum
+    {
+      id: info["id"],
+      name: info["name"],
+      type: info["type"],
+      dimension: info["dimension"],
+      popularity: sum}
+  end
   popularity
 end
 
 def otimizar_travel_stops(travel_stops)
-  travel_stops.sort_by! do |stop|
-    [stop[:dimension], -stop[:popularity], stop[:name]]
-  end
 
-  travel_stops.map { |stop| stop[:id] }
 end
 
 def query_rules(optimize, expand, ids)
@@ -72,7 +69,7 @@ def query_rules(optimize, expand, ids)
     get_expanded_info(ids)
   elsif optimize && !expand
     infos = get_expanded_info(ids)
-    calcular_popularidade(infos.to_a)
+    calc_popularity(infos.to_json)
   else
     ids
   end
